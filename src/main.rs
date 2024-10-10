91c7e5c18eaeed2fdfca09c612394577dd54ac3f
Phat <p9648213750912@gmail.com>
2024-10-10 14:59:35 +0700

    set up build script

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,16 @@ async fn main() {
     let config = config::EnvConfig::parse();
 
     let pg_pool = postgres::create_pool(&config);
+
+    tracing::info!("Postgres pool created");
+
     postgres::migrate_up(&pg_pool).await;
 
     let app = create_router(pg_pool, config);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    tracing::info!("Listening on http://0.0.0.0:3000");
 
     axum::serve(listener, app).await.unwrap();
 }
